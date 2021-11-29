67a18b3ffba71e3e82e39546aaca86f2a31f88cd
Roxanne Meuse <roxannemeuse@Roxannes-MacBook-Air.local>
2021-11-29 16:38:48 -0500

    patient form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,66 +1,165 @@
 import React from "react";
+import { useState } from "react";
 
 function Form() {
 
-    // const handleSubmit() {
+  const [bristol, setBristol] = useState()
+  const [blood, setBlood] = useState();
+  const [pain, setPain] = useState()
+  const [poopDate, setPoopDate] = useState()
 
-    // }
+//   function handleSubmit() {
+//       event.preventDefault
+//   }
 
   return (
     <div>
       Patient Form page This is where the patient will fill out a questionnaire
       daily. When they submit, the data will go to the database!
       <br />
+      <button>I did not have a bowel movement today</button>
       <form>
-        <label>
-          1. Based on the Bristol Stool Scale (shown to the right), what type of
-          poop did you have?
-          <select>
-            <option value="type1">Type 1: Separate hard lumps</option>
-            <option value="type2">Type 2: Lumpy and sausage-like</option>
-            <option value="type3">
-              Type 3: A sausage shape with cracks in the surface
-            </option>
-            <option value="type4">
-              Type 4: Like a smooth, soft sausage or snake
-            </option>
-            <option value="type5">
-              Type 5: Soft blobs with clear-cut edges
-            </option>
-            <option value="type6">
-              Type 6: Mushy consistency with ragged edges
-            </option>
-            <option value="type7">
-              Type 7: Liquid consistency with no solid pieces
-            </option>
-          </select>
-        </label>
-<br />
-        <label>
-          2. Was there any blood in your stool or on the toilet paper?
-          <select>
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </label>
-<br />
-        <label>
-          3. Based on this pain scale, how much pain did you feel around this
-          bowel movement?
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </label>
-        {/* <button onClick={handleSubmit}>Submit</button> */}
+          <div className="formQuestion">
+              <label>Date of BM: 
+                  <input type="date" name="BMdate" onChange={ (e) => {setPoopDate(e.target.value)}}/>
+              </label>
+              </div>
+        <div className="formQuestion">
+          <label>
+            1. Based on the Bristol Stool Scale (shown to the right), what type
+            of poop did you have?
+            <input type="radio" value="1" name="bristol" checked={bristol === "1"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 1: Separate hard lumps
+            <input type="radio" value="2" name="bristol" checked={bristol === "2"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 2: Lumpy and sausage-like
+            <input type="radio" value="3" name="bristol" checked={bristol === "3"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 3: A sausage shape with cracks in the surface
+            <input type="radio" value="4" name="bristol" checked={bristol === "4"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 4: Like a smooth, soft sausage or snake
+            <input type="radio" value="5" name="bristol" checked={bristol === "5"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 5: Soft blobs with clear-cut edges
+            <input type="radio" value="6" name="bristol" checked={bristol === "6"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 6: Mushy consistency with ragged edges
+            <input type="radio" value="7" name="bristol" checked={bristol === "7"} onChange={ (e) => {setBristol(e.target.value)}}/>
+            Type 7: Liquid consistency with no solid pieces
+          </label>
+        </div>
+
+        <br />
+
+        <div className="formQuestion">
+          <label>
+            2. Was there any blood in your stool or on the toilet paper?
+            <input
+              type="radio"
+              value="yes"
+              name="blood"
+              checked={blood === "yes"}
+              onChange={(e) => {
+                setBlood(e.target.value);
+              }}
+            />
+            Yes
+            <input
+              type="radio"
+              value="no"
+              name="blood"
+              checked={blood === "no"}
+              onChange={(e) => {
+                setBlood(e.target.value);
+              }}
+            />
+            No
+          </label>
+        </div>
+        <br />
+        <div className="formQuestion">
+          <label>
+            3. Based on this pain scale, how much pain did you feel around this
+            bowel movement?
+            <input type="radio" 
+            value="1"
+            name="pain"
+            checked={pain === "1"}
+            onChange={(e) => {
+              setPain(e.target.value)
+            }} />
+            1
+            <input type="radio" 
+            value="2"
+            name="pain"
+            checked={pain === "2"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            2
+            <input type="radio" 
+            value="3" 
+            name="pain"
+            checked={pain === "3"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            3
+            <input type="radio" 
+            value="4" 
+            name="pain"
+            checked={pain === "4"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            4
+            <input type="radio" 
+            value="5" 
+            name="pain"
+            checked={pain === "5"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            5
+            <input type="radio" 
+            value="6" 
+            name="pain"
+            checked={pain === "6"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            6
+            <input type="radio" 
+            value="7" 
+            name="pain"
+            checked={pain === "7"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            7
+            <input type="radio" 
+            value="8" 
+            name="pain"
+            checked={pain === "8"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            8
+            <input type="radio" 
+            value="9" 
+            name="pain"
+            checked={pain === "9"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            9
+            <input type="radio" 
+            value="10" 
+            name="pain"
+            checked={pain === "10"}
+            onChange={(e) => {
+              setPain(e.target.value)}}
+            />
+            10
+          </label>
+        </div>
+        <button type="submit" >Submit</button>
       </form>
     </div>
   );
