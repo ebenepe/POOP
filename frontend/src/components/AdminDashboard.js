0170bcc80aed9c37131deb3ba5da2251faa9d54d
Dylan Pruitt <pruitt.dylan@gmail.com>
2021-12-01 16:39:45 -0500

    Dashboard database pull added

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { db, auth } from "../firebase-config";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 function AdminDashboard() {
+    const [records, setRecords] = useState([]);
+    const usersCollectionRef = collection(db, "data");
+
+    // This area is to read all of the entries on the DB
+  // and will most likely be moved to the admin dashboard
+  // R is for READ (all)
+  // loads data when page is loaded
+  useEffect(() => {
+    const getEntries = async () => {
+      const data = await getDocs(usersCollectionRef);
+      // console.log('data:')
+      // console.log(data) // for testing purposes
+      setRecords(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); // for testing purposes
+    };
+    getEntries();
+  }, []);
+  // **********************************
+
     return (
         <div>
             Admin Dashboard Page
