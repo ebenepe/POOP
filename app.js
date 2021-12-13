bcda8a64c909b3c52fdda63225142adce1f4709b
Erik Benepe <ebenepe@gmail.com>
2021-12-13 14:09:35 -0500

    no helmet

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const helmet = require('helmet')
+// const helmet = require('helmet')
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "/frontend/build/index.html")));
 
-app.use(
-  // [
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      connectSrc: ["'self'", 'https://checkout.stripe.com'],
-      frameSrc: ["'self'", 'https://checkout.stripe.com'],
-      childSrc: ["'self'", 'https://checkout.stripe.com'],
-      scriptSrc: ["'self'", 'https://checkout.stripe.com'],
-      styleSrc: [
-        "'self'",
-        'https://fonts.googleapis.com',
-        'https://checkout.stripe.com',
-      ],
-      fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-      imgSrc: ["'self'", 'https://*.stripe.com', 'https://res.cloudinary.com'],
-      baseUri: ["'self'"],
-    },
-  })
-  // ]
-)
+// app.use(
+//   // [
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       connectSrc: ["'self'", 'https://checkout.stripe.com'],
+//       frameSrc: ["'self'", 'https://checkout.stripe.com'],
+//       childSrc: ["'self'", 'https://checkout.stripe.com'],
+//       scriptSrc: ["'self'", 'https://checkout.stripe.com'],
+//       styleSrc: [
+//         "'self'",
+//         'https://fonts.googleapis.com',
+//         'https://checkout.stripe.com',
+//       ],
+//       fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+//       imgSrc: ["'self'", 'https://*.stripe.com', 'https://res.cloudinary.com'],
+//       baseUri: ["'self'"],
+//     },
+//   })
+//   // ]
+// )
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
