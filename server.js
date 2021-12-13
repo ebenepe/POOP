cab5b125e57ecbfb9814e213880324d986e003f3
Erik Benepe <ebenepe@gmail.com>
2021-12-13 11:29:43 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
