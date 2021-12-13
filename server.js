bdefda17f74bb8fef006300f50665d3f302d4cca
Erik Benepe <ebenepe@gmail.com>
2021-12-13 11:36:49 -0500

    added listen

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("server is running on port " + PORT);
 });
