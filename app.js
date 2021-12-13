5b71f8b8ae008afd45e18b7c99119ad72a3aa569
Erik Benepe <ebenepe@gmail.com>
2021-12-13 14:28:26 -0500

    fixed server issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "/frontend/build/index.html")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 // app.use(
 //   // [
